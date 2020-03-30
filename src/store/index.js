3938ab5fc459ce2dce8412d12703c9632962cbbd
Aleksey <alekcyvolkov@gmail.com>
2020-03-30 18:17:09 +0300

    login form

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,27 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    userIsAuth: false,
+    userRole: null,
+    userName: null,
   },
   mutations: {
+    setUserIsAuth(state, isAuth) {
+      state.userIsAuth = isAuth
+    },
+    setUserRole(state, role) {
+      state.userRole = role
+    },
+    setUserName(state, userName) {
+      state.userName = userName
+    }
   },
   actions: {
+    login ({ commit }, payload) {
+      commit('setUserIsAuth', true)
+      commit('setUserRole', payload.role)
+      commit('setUserName', payload.userName)
+    }
   },
   modules: {
   }
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,3 +1,118 @@
 <template>
-  <div>login</div>
+  <div class="container">
+    <div class="columns">
+      <div class="column is-one-third-desktop is-two-thirds-tablet is-full login-form">
+        {{isAuth ? '22' : '11'}}
+        <div class="field">
+          <label class="label">Username (email)</label>
+          <div class="control">
+            <input
+              class="input"
+              v-bind:class="{'is-danger': emailError !== null}"
+              type="text"
+              placeholder="Text input"
+              v-model="email"
+            >
+          </div>
+          <p v-if="emailError" class="help is-danger">{{emailError}}</p>
+        </div>
+        <div class="field">
+          <label class="label">Password</label>
+          <div class="control">
+            <input
+              class="input"
+              v-model="password"
+              v-bind:class="{'is-danger': passwordError !== null}"
+              type="password"
+              placeholder="password"
+            >
+          </div>
+          <p v-if="passwordError" class="help is-danger">{{passwordError}}</p>
+        </div>
+        <button v-on:click="login" class="button is-primary">Log In</button>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import { apiUrl } from '@/apiConfig.js'
+import { validateEmail } from '@/utils/regex.js'
+import store from '@/store'
+
+export default {
+  name: 'LoginPage',
+  data: function () {
+    return {
+      email: '',
+      password: '',
+      emailError: null,
+      passwordError: null,
+    }
+  },
+  computed: {
+    isAuth() {
+      return store.state.userIsAuth
+    }
+  },
+  methods: {
+    login: function () {
+      if (this.validateForm()) {
+        fetch(`${apiUrl}/users?login=${this.email}`)
+          .then(response => response.json().then(data => {
+            if (this.validateUsersResponse(data)) {
+              store.dispatch('login', {
+                role: data[0].role,
+                userName: data[0].login,
+              })
+              this.$router.push('/')
+            }
+          }))
+      }
+    },
+    validateForm: function () {
+      let isValid = true;
+      this.emailError = null;
+      this.passwordError = null;
+      if (this.email === '') {
+        isValid = false;
+        this.emailError = 'The field must not be empty';
+      } else {
+        if (!validateEmail(this.email)) {
+          isValid = false;
+          this.emailError = 'Enter valid email';
+        }
+      }
+      if (this.password === '') {
+        isValid = false;
+        this.passwordError = 'The field must not be empty';
+      }
+      return isValid;
+    },
+    validateUsersResponse: function (data) {
+      let isValid = true;
+      if (!data || data.length === 0) {
+        isValid = false;
+        this.emailError = 'User with this email does not exist'
+      } else {
+        if (String(data[0].password) !== this.password) {
+          isValid = false;
+          this.passwordError = 'Password incorrect'
+        }
+      }
+      return isValid;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.login-form {
+  text-align: left;
+  margin-top: 100px;
+}
+.columns {
+  margin: 0;
+  justify-content: center;
+}
+</style>
